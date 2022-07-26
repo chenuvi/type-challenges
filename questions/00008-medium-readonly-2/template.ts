3cba793de0630a77a9d4f5aeb8a824ab250c3ae8
kun.chen <kun.chen@yintech.cn>
2022-07-26 18:46:25 +0800

    Feat: readonly

--- questions/00008-medium-readonly-2/template.ts
+++ questions/00008-medium-readonly-2/template.ts
@@ -1,1 +1,6 @@
-type MyReadonly2<T, K> = any
+type MyReadonly2<T, K extends keyof T = keyof T> = Omit<T, K> & {
+  readonly [P in K]: T[P]
+}
+
+// type MyReadonly2<T, K extends keyof T = keyof T> = Readonly<Pick<T, K>> &
+//   Omit<T, K>
