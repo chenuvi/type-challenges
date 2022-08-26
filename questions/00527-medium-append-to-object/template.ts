bf09db6b948b060b2847df54bbca495d68b4c53a
kun.chen <kun.chen@yintech.cn>
2022-08-26 12:04:37 +0800

    Feat: [M]  AppendToObject(对象添加新属性)

--- questions/00527-medium-append-to-object/template.ts
+++ questions/00527-medium-append-to-object/template.ts
@@ -1,1 +1,4 @@
-type AppendToObject<T, U, V> = any
+type basicKeyType = string | number | symbol
+type AppendToObject<T, K extends basicKeyType, V> = {
+  [P in keyof T | K]: P extends keyof T ? T[P] : V
+}
