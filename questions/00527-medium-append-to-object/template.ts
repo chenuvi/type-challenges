33b7772561ea66d3da9b5e034007b8d22a752af5
chenuvi <chenuvi@outlook.com>
2022-08-28 23:06:20 +0800

    Feat: [M] append-to-object

--- questions/00527-medium-append-to-object/template.ts
+++ questions/00527-medium-append-to-object/template.ts
@@ -1,1 +1,4 @@
-type AppendToObject<T, U, V> = any
+type KeyType = string | number | symbol
+type AppendToObject<T extends object, K extends KeyType, V> = {
+    [P in keyof T | K]: P extends keyof T ? T[P] : V
+}
