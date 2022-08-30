327c566fb8142a4c68f95daa2f184e5fd65ffcff
chenuvi <chenuvi@outlook.com>
2022-08-30 19:32:21 +0800

    Feat:[M] kebabcase

--- questions/00612-medium-kebabcase/template.ts
+++ questions/00612-medium-kebabcase/template.ts
@@ -1,1 +1,6 @@
-type KebabCase<S> = any
+type KebabCase<S extends string
+  > = S extends `${infer F}${infer Rest}`
+  ? Rest extends Uncapitalize<Rest>
+    ? `${Uncapitalize<F>}${KebabCase<Rest>}`
+    : `${Uncapitalize<F>}-${KebabCase<Rest>}`
+  : S
