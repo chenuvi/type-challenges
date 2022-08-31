2b69e21f6a2bb06a7b41175601f95a08ce99e46c
chenuvi <chenuvi@outlook.com>
2022-08-31 22:13:20 +0800

    Feat: [M] diff

--- questions/00645-medium-diff/template.ts
+++ questions/00645-medium-diff/template.ts
@@ -1,1 +1,4 @@
-type Diff<O, O1> = any
+type DiffKeys<T, U> = Exclude<keyof T | keyof U, keyof (T | U)>
+type Diff<T, U> = {
+  [K in DiffKeys<T, U>]: K extends keyof T ? T[K] : K extends keyof U ? U[K] : never
+}
