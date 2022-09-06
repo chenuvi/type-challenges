1cfb62945bda6654d691e14a8fbee0ded921e696
kun.chen <kun.chen@yintech.cn>
2022-09-06 13:02:53 +0800

    Docs: note question

--- questions/00949-medium-anyof/template.ts
+++ questions/00949-medium-anyof/template.ts
@@ -2,6 +2,7 @@ type FalseType = 0 | "" | false | [] | { [key: string]: never }
 type AnyOf<T extends readonly any[]> = T[number] extends FalseType
   ? false
   : true
+  // TODO AnyOf 为什么有一个 为真就返回全为真
 // my solution ,too complicated
 // type AnyOf<T extends readonly any[]> = T extends [infer F, ...infer R]
 //   ? F extends FalseType
