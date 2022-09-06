0140ebe83d6b427a7fecda103a751bc44ed31a81
kun.chen <kun.chen@yintech.cn>
2022-09-06 12:59:15 +0800

    Feat: [M] AnyOf

--- questions/00949-medium-anyof/template.ts
+++ questions/00949-medium-anyof/template.ts
@@ -1,1 +1,10 @@
-type AnyOf<T extends readonly any[]> = any
+type FalseType = 0 | "" | false | [] | { [key: string]: never }
+type AnyOf<T extends readonly any[]> = T[number] extends FalseType
+  ? false
+  : true
+// my solution ,too complicated
+// type AnyOf<T extends readonly any[]> = T extends [infer F, ...infer R]
+//   ? F extends FalseType
+//     ? AnyOf<R>
+//     : true
+//   : false
