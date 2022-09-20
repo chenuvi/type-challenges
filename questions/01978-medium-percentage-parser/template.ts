d7539a9dcb9d2c25aa3b8236fe3b8a23f3cbd61a
chenuvi <chenuvi@outlook.com>
2022-09-20 23:16:04 +0800

    Feat: [M] PercentageParser

--- questions/01978-medium-percentage-parser/template.ts
+++ questions/01978-medium-percentage-parser/template.ts
@@ -1,1 +1,5 @@
-type PercentageParser<A extends string> = any
+type CheckPrefix<S extends string> = S extends '+' | '-' ? S : never
+type CheckSuffix<S extends string> = S extends `${infer L}%` ? [L, '%'] : [S, '']
+type PercentageParser<S extends string> = S extends `${CheckPrefix<infer L>}${infer R}`
+  ? [L, ...CheckSuffix<R>]
+  : ['', ...CheckSuffix<S>]
